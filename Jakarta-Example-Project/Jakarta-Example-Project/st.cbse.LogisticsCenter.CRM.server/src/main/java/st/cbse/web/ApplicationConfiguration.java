17ac091cabb624afc46ec4fb47ff06441425e3de
YouennBogaer <163859975+YouennBogaer@users.noreply.github.com>
2025-07-02 15:54:20 +0200

    applicationConfig

---
+++ Jakarta-Example-Project/Jakarta-Example-Project/st.cbse.LogisticsCenter.CRM.server/src/main/java/st/cbse/web/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package st.cbse.web;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("rest-api")
+public class ApplicationConfiguration extends Application {
+}
